a4f9884ed1599f428a1dda77bd6df376f10c7fff
Jonasoft <carlosucananbarrera@hotmail.com>
2019-12-29 21:11:23 -0500

    servidor montado

---
+++ server.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 8 */
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+process.env.PORT = process.env.PORT || 3000;
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+
+app.listen(process.env.PORT, async() => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
